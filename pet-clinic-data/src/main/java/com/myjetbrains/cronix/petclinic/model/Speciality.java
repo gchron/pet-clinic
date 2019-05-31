b69fb95f10d68a256b3da2847e6cc7a8e23259d1
cronix <grzegorz.chron@gmail.com>
2019-05-31 17:54:40 +0200

    added PetTypeService and PetTypeMapService

---
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.myjetbrains.cronix.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.myjetbrains.cronix.petclinic.services;
+
+import com.myjetbrains.cronix.petclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long> {
+
+}
