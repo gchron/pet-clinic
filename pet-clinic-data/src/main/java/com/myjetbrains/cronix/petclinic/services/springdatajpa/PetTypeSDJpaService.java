db60205a94a0197979840a9258b7a033484e71d9
cronix <grzegorz.chron@gmail.com>
2019-06-05 15:10:47 +0200

    added petType and vet spring data jpa service

---
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package com.myjetbrains.cronix.petclinic.services.springdatajpa;
+
+import com.myjetbrains.cronix.petclinic.model.PetType;
+import com.myjetbrains.cronix.petclinic.repositories.PetTypeRepository;
+import com.myjetbrains.cronix.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(t -> types.add(t));
+        return types;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
