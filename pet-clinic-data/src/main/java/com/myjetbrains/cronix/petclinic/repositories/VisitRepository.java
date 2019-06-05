50fd419ea7feab03c79381ab384bcb7d6015b4ef
cronix <grzegorz.chron@gmail.com>
2019-06-05 14:52:27 +0200

    added owner spring data jpa service, added method to owner repository, fixes in visit repository

--- pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/repositories/VisitRepository.java
@@ -1,6 +1,7 @@
 package com.myjetbrains.cronix.petclinic.repositories;
 
+import com.myjetbrains.cronix.petclinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VisitRepository extends CrudRepository<VisitRepository, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
