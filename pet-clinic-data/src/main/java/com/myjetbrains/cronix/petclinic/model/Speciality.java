a4b6e53eca18fc588f7324046ddee8b60e40f5fd
cronix <grzegorz.chron@gmail.com>
2019-06-05 11:15:52 +0200

    updated pet, visit, vet, speciality entities

--- pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.myjetbrains.cronix.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String desc;
 
     public String getDesc() {
--- pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/model/Visit.java
@@ -1,11 +1,19 @@
 package com.myjetbrains.cronix.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String desc;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
