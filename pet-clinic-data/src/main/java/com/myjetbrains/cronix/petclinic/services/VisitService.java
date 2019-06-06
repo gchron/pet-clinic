43362dbd17bb08171a56391e5965c486e80e8ae9
cronix <grzegorz.chron@gmail.com>
2019-06-06 11:02:19 +0200

    added map impl for visit service

---
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.myjetbrains.cronix.petclinic.services;
+
+import com.myjetbrains.cronix.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/myjetbrains/cronix/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.myjetbrains.cronix.petclinic.services.map;
+
+import com.myjetbrains.cronix.petclinic.model.Visit;
+import com.myjetbrains.cronix.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
